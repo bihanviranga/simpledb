a62908f77f6bcce7f385249f6affc811ba155102
Bihan Viranga <bihanviranga@gmail.com>
2020-05-23 02:29:43 +0530

    do_meta_command and prepare_statement

--- main.c
+++ main.c
@@ -40,6 +40,47 @@ void close_input_buffer(InputBuffer *input_buffer) {
   free(input_buffer);
 }
 
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+typedef enum {
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
 int main(int argc, char *argv[]) {
   InputBuffer *input_buffer = new_input_buffer();
   while (true) {
