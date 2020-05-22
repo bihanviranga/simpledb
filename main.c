f7371507d0592ef1fa91e2435e4387ca6e57955f
Bihan Viranga <bihanviranga@gmail.com>
2020-05-23 01:44:55 +0530

    Read input and close buffer functions

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
@@ -33,3 +35,22 @@ InputBuffer* new_input_buffer() {
     return input_buffer;
 }
 
+void print_prompt() { printf("db > "); }
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Ignore trailing newline
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
