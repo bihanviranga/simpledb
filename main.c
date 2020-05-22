759bcad0d6ad3af35ff8811ea5bc8e595ed4a5f8
Bihan Viranga <bihanviranga@gmail.com>
2020-05-23 01:48:32 +0530

    REPL done

--- main.c
+++ main.c
@@ -3,22 +3,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char* argv[]) {
-    InputBuffer* input_buffer = new_input_buffer();
-    while (true) {
-        print_prompt();
-        read_input(input_buffer);
-
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }
-        else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-        }
-    }
-}
+#include <string.h>
 
 typedef struct {
     char* buffer;
@@ -53,4 +38,20 @@ void read_input(InputBuffer* input_buffer) {
 void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
 }
