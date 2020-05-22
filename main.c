43d771a05757bee2cb931c3e43a02a54918c564a
Bihan Viranga <bihanviranga@gmail.com>
2020-05-23 01:32:37 +0530

    Add InputBuffer struct

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/types.h>
+
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
@@ -13,3 +17,19 @@ int main(int argc, char* argv[]) {
         }
     }
 }
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
