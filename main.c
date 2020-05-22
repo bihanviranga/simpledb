6a1e24eed2ed96a0a6b4b3764fd07e9578ae3148
Bihan Viranga <bihanviranga@gmail.com>
2020-05-23 02:17:30 +0530

    Refactor main function

--- main.c
+++ main.c
@@ -1,57 +1,71 @@
 #include <stdbool.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
-typedef struct {
-    char* buffer;
-    size_t buffer_length;
-    ssize_t input_length;
+typedef struct
+{
+  char *buffer;
+  size_t buffer_length;
+  ssize_t input_length;
 } InputBuffer;
 
-InputBuffer* new_input_buffer() {
-    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
-    input_buffer->input_length = 0;
+InputBuffer *new_input_buffer() {
+  InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
 
-    return input_buffer;
+  return input_buffer;
 }
 
 void print_prompt() { printf("db > "); }
 
-void read_input(InputBuffer* input_buffer) {
-    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+void read_input(InputBuffer *input_buffer) {
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-    if (bytes_read <= 0) {
-        printf("Error reading input.\n");
-        exit(EXIT_FAILURE);
-    }
+  if (bytes_read <= 0) {
+    printf("Error reading input.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    // Ignore trailing newline
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
+void close_input_buffer(InputBuffer *input_buffer) {
+  free(input_buffer->buffer);
+  free(input_buffer);
 }
 
-int main(int argc, char* argv[]) {
-    InputBuffer* input_buffer = new_input_buffer();
-    while (true) {
-        print_prompt();
-        read_input(input_buffer);
+int main(int argc, char *argv[]) {
+  InputBuffer *input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }
-        else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-        }
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'\n", input_buffer->buffer);
+        continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
+  }
 }
