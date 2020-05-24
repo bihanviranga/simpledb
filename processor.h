e0d5e74aff41e85a6d24e8e0a396dd017fef520c
Bihan Viranga <bihanviranga@gmail.com>
2020-05-24 23:10:01 +0530

    Add serialize_row and deserialize_row

--- processor.h
+++ processor.h
@@ -41,4 +41,18 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 
 void execute_statement(Statement* statement);
 
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+void serialize_row(Row* source, void* destination);
+
+void deserialize_row(void* source, Row* destination);
+
 #endif
