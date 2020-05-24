82d87e756f94f9f7ae3c34a972163219e9a88dde
Bihan Viranga <bihanviranga@gmail.com>
2020-05-24 23:16:44 +0530

    Table struct

--- processor.h
+++ processor.h
@@ -55,4 +55,14 @@ void serialize_row(Row* source, void* destination);
 
 void deserialize_row(void* source, Row* destination);
 
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+  uint32_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+} Table;
+
 #endif
