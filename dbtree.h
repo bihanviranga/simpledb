5d4fb9ecf90b1a97a41ed43ee8c55e8e03c7e5a6
Bihan Viranga <bihanviranga@gmail.com>
2020-06-02 20:48:59 +0530

    Start work on the tree representation

---
+++ dbtree.h
@@ -0,0 +1,12 @@
+/******************************************************************************** 
+ * dbtree.h : B+ tree implementation
+ ********************************************************************************/
+#ifndef _DBTREE_H
+#define _DBTREE_H
+
+typedef enum {
+  NODE_INTERNAL,
+  NODE_LEAF
+} NodeType;
+
+#endif
