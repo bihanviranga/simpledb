507946913e6cef68a7713a6f032180f6931bbaaf
Bihan Viranga <bihanviranga@gmail.com>
2020-06-13 00:34:55 +0530

    Outline for creating new root node

--- internals.c
+++ internals.c
@@ -560,3 +560,27 @@ void set_node_type(void* node, NodeType type) {
   uint8_t value = type;
   *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
 }
+
+/*
+ * Old root is copied to a new page, and becomes the left child.
+ * New root points to the two leaf (child) nodes.
+ */
+void create_new_root(Table* table, uint32_t right_child_page_num) {
+  void* root = get_page(table->pager, table->root_page_num);
+  void* right_child = get_page(table->pager, right_child_page_num);
+  uint32_t left_child_page_number = get_unused_page_num(table->pager);
+  void* left_child = get_page(table->pager, left_child_page_number);
+
+  /* Copy the old root to left child */
+  memcpy(left_child, root, PAGE_SIZE);
+  set_node_root(left_child, false);
+
+  /* Root node is now an internal node, with one key and two children */
+  initialize_internal_node(root);
+  set_node_root(root, true);
+  *internal_node_num_keys(root) = 1;
+  *internal_node_child(root, 0) = left_child_page_num;
+  uint32_t left_child_max_key = get_node_max_key(left_child);
+  *internal_node_key(root, 0) = left_child_max_key;
+  *internal_node_right_child(root) = right_child_page_num;
+}
